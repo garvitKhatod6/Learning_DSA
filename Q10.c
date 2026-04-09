78419018cb6193e7828869d0eb27c2f5dc29b53e
garvitKhatod6 <222789847+garvitKhatod6@users.noreply.github.com>
2026-04-09 23:42:37 +0530

    Refactor Q10.c to improve code formatting

--- Q10.c
+++ Q10.c
@@ -1,4 +1,4 @@
-/*Problem: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
+/*Q9: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
 
 Input:
 - Single line: a lowercase string containing only alphabetic characters (no spaces)
@@ -14,6 +14,10 @@ Output:
 olleh
 
 Explanation: The first character moves to the last position, the second to the second-last, and so on until the entire string is mirrored*/
+
+
+
+
 #include <stdio.h>
 int main(){
 int i,j;
