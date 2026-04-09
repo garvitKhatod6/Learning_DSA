3889f6064c06d4b92fa823bf2e812d87fa23d95b
garvitKhatod6 <222789847+garvitKhatod6@users.noreply.github.com>
2026-04-09 23:44:27 +0530

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -1,4 +1,4 @@
-/*Q9: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
+/*Q10: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
 
 Input:
 - Single line: a lowercase string containing only alphabetic characters (no spaces)
